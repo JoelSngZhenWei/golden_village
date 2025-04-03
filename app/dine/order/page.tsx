c96484ad19d941bf8932ff24f961b38220a2d2b1
JoelSngZhenWei <joelsngzw@gmail.com>
2025-04-04 02:11:44 +0800

    changes

---
+++ app/dine/order/page.tsx
@@ -0,0 +1,14 @@
+import { BottomNav } from "@/components/bottom-nav"
+import { Header } from "@/components/header"
+
+export default function order() {
+  return (
+    <section className="pb-20">
+      <Header />
+
+      profile details
+      <BottomNav />
+    </section>
+  )
+}
+
